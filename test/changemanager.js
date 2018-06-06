f01470294ec50688a291233c97cb53c09f3e3c0c
David Peherstorfer <david@peherstorfer.info>
2018-06-06 16:55:26 +0200

    Fixed .babelrc to target current node version and convert all tests to async tests for better readability

--- test/changemanager.js
+++ test/changemanager.js
@@ -17,96 +17,92 @@ let State = {
 };
 
 contract('ChangeManager', function (accounts) {
-    it("should create a new ChangeRequest", function () {
-        return ChangeManager.deployed().then(function (changemanager) {
-            return changemanager.createNewChangeRequest(gitHashFirst, additionalInformation, costs, estimation);
-        }).then(function (result) {
-            result.logs.filter(log => log.event === "NewChangeRequest")
-                .map(log => log.args)
-                .forEach(args => {
-                    // console.log("Args1: ", args);
-                    changeRequestAddressFirst = args._changeRequestAddress;
-                    assert.equal(args._gitHash, gitHashFirst, 'Correct git hash has not been returned');
-                    assert.equal(args._additionalInformation, additionalInformation, 'Correct additional information have not been returned');
-                    assert.equal(args._costs, costs, 'Corrects costs have not been returned');
-                    assert.equal(args._estimation, estimation, 'Correct estimation has not been returned');
-                    assert.isNotNull(changeRequestAddressFirst, "ChangeRequest contract address was null");
-                });
-        });
-    });
-
-    it("should get the data of the newly created ChangeRequest", function () {
-        changerequestFirst = ChangeRequest.at(changeRequestAddressFirst);
-        return changerequestFirst.viewChange().then(function (result) {
-            assert.equal(result[0], gitHashFirst, 'Correct git hash has not been returned');
-            assert.equal(result[1], additionalInformation, 'Correct additional information have not been returned');
-            assert.equal(result[2], costs, 'Corrects costs have not been returned');
-            assert.equal(result[3], estimation, 'Correct estimation has not been returned');
-        });
-    });
-
-    it("should vote the newly created ChangeRequest down", function () {
-        return changerequestFirst.managementVote(false, [], "This is the reason why the change request is being rejected", {from: accounts[0]})
-            .then(function (result) {
-                result.logs.filter(log => log.event === "NewVote")
-                    .map(log => log.args)
-                    .forEach(args => {
-                        assert.equal(args._vote, false, 'The vote has not been downvoted');
-                        assert.equal(args._currentState, State.changeRejected, 'The change has not been rejected');
-                        assert.equal(args._votesLeft, 0, 'The votes have not been reduced to 0');
-                    });
+    it("should create a new ChangeRequest", async function () {
+        let changemanager = await  ChangeManager.deployed()
+        let result = await changemanager.createNewChangeRequest(gitHashFirst, additionalInformation, costs, estimation);
+        result.logs.filter(log => log.event === "NewChangeRequest")
+            .map(log => log.args)
+            .forEach(args => {
+                // console.log("Args1: ", args);
+                changeRequestAddressFirst = args._changeRequestAddress;
+                assert.equal(args._gitHash, gitHashFirst, 'Correct git hash has not been returned');
+                assert.equal(args._additionalInformation, additionalInformation, 'Correct additional information have not been returned');
+                assert.equal(args._costs, costs, 'Corrects costs have not been returned');
+                assert.equal(args._estimation, estimation, 'Correct estimation has not been returned');
+                assert.isNotNull(changeRequestAddressFirst, "ChangeRequest contract address was null");
             });
     });
 
-    it("should not be able to vote on a downvoted ChangeRequest", function () {
-        return changerequestFirst.responsibleVote(true, "This is the reason why the change request is being rejected", {from: accounts[1]})
+    it("should get the data of the newly created ChangeRequest", async function () {
+        changerequestFirst = ChangeRequest.at(changeRequestAddressFirst);
+        let result = await changerequestFirst.viewChange();
+        assert.equal(result[0], gitHashFirst, 'Correct git hash has not been returned');
+        assert.equal(result[1], additionalInformation, 'Correct additional information have not been returned');
+        assert.equal(result[2], costs, 'Corrects costs have not been returned');
+        assert.equal(result[3], estimation, 'Correct estimation has not been returned');
+    });
+
+    it("should vote the newly created ChangeRequest down", async function () {
+        let result = await changerequestFirst.managementVote(false, [], "This is the reason why the change request is being rejected", {from: accounts[0]})
+        result.logs.filter(log => log.event === "NewVote")
+            .map(log => log.args)
+            .forEach(args => {
+                assert.equal(args._vote, false, 'The vote has not been downvoted');
+                assert.equal(args._currentState, State.changeRejected, 'The change has not been rejected');
+                assert.equal(args._votesLeft, 0, 'The votes have not been reduced to 0');
+            });
+    });
+
+    it("should not be able to vote on a downvoted ChangeRequest", async function () {
+        await changerequestFirst.responsibleVote(true, "This is the reason why the change request is being rejected", {from: accounts[1]})
             .catch(error => {
                 assert.include(error.message, 'revert', 'The vote has not been reverted');
             });
     });
 
-    it("should create a new ChangeRequest and be accepted by all parties", function () {
-        return ChangeManager.deployed().then(function (changemanager) {
-            return changemanager.createNewChangeRequest(gitHashSecond, additionalInformation, costs, estimation);
-        }).then(function (result) {
+    it("should create a new ChangeRequest and be accepted by all parties", async function () {
 
-            result.logs.filter(log => log.event === "NewChangeRequest")
+        // Create new ChangeRequest
+        let changemanager = await ChangeManager.deployed();
+        let result = await changemanager.createNewChangeRequest(gitHashSecond, additionalInformation, costs, estimation);
+        result.logs.filter(log => log.event === "NewChangeRequest")
+            .map(log => log.args)
+            .forEach(args => {
+                // console.log("Args2: ", args);
+                changeRequestAddressSecond = args._changeRequestAddress;
+            });
+        changerequestSecond = ChangeRequest.at(changeRequestAddressSecond);
+
+        // Manage the new ChangeRequest and set Account 1 + 2 as responsible Parties
+        result = await changerequestSecond.managementVote(true, [accounts[1], accounts[2]], "", {from: accounts[0]})
+        result.logs.filter(log => log.event === "NewVote")
+            .map(log => log.args)
+            .forEach(args => {
+                // console.log("Args3: ", args);
+                assert.equal(args._currentState, State.changeManaged, 'The change has not been rejected');
+            });
+
+        // Illegally try to vote from Account 4
+        changerequestSecond.responsibleVote(true, "", {from: accounts[4]})
+            .catch(error => {
+                assert.include(error.message, 'revert', 'An unallowed party was able to vote');
+            });
+
+        // Accept change using Account 1 + 2
+        await changerequestSecond.responsibleVote(true, "", {from: accounts[2]});
+        result = await changerequestSecond.responsibleVote(true, "", {from: accounts[1]});
+        result.logs.filter(log => log.event === "NewVote")
+            .map(log => log.args)
+            .forEach(args => {
+                // console.log("Args5: ", args);
+            });
+
+        // Release the Change
+        result = await changerequestSecond.releaseChange().then(function (result) {
+            result.logs.filter(log => log.event === "NewVote")
                 .map(log => log.args)
                 .forEach(args => {
-                    // console.log("Args2: ", args);
-                    changeRequestAddressSecond = args._changeRequestAddress;
-                });
-
-            changerequestSecond = ChangeRequest.at(changeRequestAddressSecond);
-            return changerequestSecond.managementVote(true, [accounts[1], accounts[2]], "", {from: accounts[0]})
-                .then(function (result) {
-                    result.logs.filter(log => log.event === "NewVote")
-                        .map(log => log.args)
-                        .forEach(args => {
-                            // console.log("Args3: ", args);
-                            assert.equal(args._currentState, State.changeManaged, 'The change has not been rejected');
-                        });
-                    changerequestSecond.responsibleVote(true, "", {from: accounts[4]})
-                        .catch(error => {
-                            assert.include(error.message, 'revert', 'An unallowed party was able to vote');
-                        });
-                    changerequestSecond.responsibleVote(true, "", {from: accounts[2]})
-                        .then();
-                    changerequestSecond.responsibleVote(true, "", {from: accounts[1]})
-                        .then(function (result) {
-                            result.logs.filter(log => log.event === "NewVote")
-                                .map(log => log.args)
-                                .forEach(args => {
-                                    // console.log("Args5: ", args);
-                                });
-                        });
-                    return changerequestSecond.releaseChange().then(function (result) {
-                        result.logs.filter(log => log.event === "NewVote")
-                            .map(log => log.args)
-                            .forEach(args => {
-                                // console.log("Args5: ", args);
-                            });
-                    });
+                    // console.log("Args5: ", args);
                 });
         });
     });

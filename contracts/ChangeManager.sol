eee39aad06f65351ea65149d7b1b671790c2067c
David Peherstorfer <david@peherstorfer.info>
2018-06-27 13:29:39 +0200

    Try to only save state on blockchain

--- contracts/ChangeManager.sol
+++ contracts/ChangeManager.sol
@@ -1,13 +1,13 @@
 pragma solidity ^0.4.24;
-pragma experimental ABIEncoderV2;
 
 import {ChangeTracker} from './ChangeTracker.sol';
 import {ChangeRequest} from './ChangeRequest.sol';
 
 // This contracts serves as a ChangeRequest factory
-contract ChangeManager {
+contract ChangeManager is ChangeTracker{
     address private _constructionManager;
     ChangeRequest[] private _changes;
+    mapping(bytes20 => ChangeRequest) _changeRequests;
 
     event NewChangeRequest(
         address _changeRequestAddress,
@@ -17,17 +17,131 @@ contract ChangeManager {
         uint256 _estimation
     );
 
+    // This event gets propagated every time a new Vote happens and tracks the _currentChange.state and the _votesLeft
+    event NewVote(
+        address _voter,
+        bool _vote,
+        State _currentState,
+        string _voteInfo,
+        uint256 _votesLeft
+    );
+
     constructor() public {
         _constructionManager = msg.sender;
     }
 
     // Creates a new ChangeRequest contract and saves the address in _changes
     function createNewChangeRequest(
-        bytes20 gitHash, string additionalInformation, uint256 costs, uint256 estimation
-    ) public
+        bytes20 gitHash,
+        string additionalInformation,
+        uint256 costs,
+        uint256 estimation
+    )
+    public
     {
-        ChangeRequest changerequest = new ChangeRequest(gitHash, additionalInformation, costs, estimation, msg.sender);
-        _changes.push(changerequest);
-        emit NewChangeRequest(changerequest, gitHash, additionalInformation, costs, estimation);
+        ChangeRequest changeRequest = new ChangeRequest(gitHash, additionalInformation, costs, estimation, msg.sender, _constructionManager);
+        _changes.push(changeRequest);
+        _changeRequests[gitHash] = changeRequest;
+        emit NewChangeRequest(changeRequest, gitHash, additionalInformation, costs, estimation);
+    }
+
+    function managementVote(
+        bytes20 gitHash,
+        bool acceptChange,
+        address[] responsibleParties,
+        string voteInfo
+    )
+    public
+    {
+        require(_changeRequests[gitHash].getState() == State.changeProposed);
+        require(msg.sender == _constructionManager);
+
+        ChangeRequest changeRequest = _changeRequests[gitHash];
+
+        if (acceptChange) {
+            changeRequest.setVoteCount(responsibleParties.length, msg.sender);
+            for (uint i = 0; i < responsibleParties.length; i++) {
+                changeRequest.setAllowedToVote(responsibleParties[i], msg.sender);
+            }
+            changeRequest.setState(State.changeManaged, msg.sender);
+
+            emit NewVote(
+                msg.sender,
+                acceptChange,
+                changeRequest.getState(),
+                changeRequest.getVoteInfo(),
+                changeRequest.getVoteCount()
+            );
+        }
+        else {
+            changeRequest.setState(State.changeRejected, msg.sender);
+            changeRequest.setVoteInfo(voteInfo, msg.sender);
+            emit NewVote(
+                msg.sender,
+                acceptChange,
+                changeRequest.getState(),
+                changeRequest.getVoteInfo(),
+                0
+            );
+        }
+    }
+
+    function responsibleVote(
+        bytes20 gitHash,
+        bool acceptChange,
+        string voteInfo
+    )
+    public
+    {
+        require(_changeRequests[gitHash].getState() == State.changeManaged);
+
+        ChangeRequest changeRequest = _changeRequests[gitHash];
+
+        require(changeRequest.isAllowedToVote(msg.sender));
+
+        if (!acceptChange) {
+            changeRequest.setState(State.changeRejected, msg.sender);
+            changeRequest.setVoteInfo(voteInfo, msg.sender);
+            emit NewVote(
+                msg.sender,
+                acceptChange,
+                changeRequest.getState(),
+                changeRequest.getVoteInfo(),
+                0
+            );
+        }
+        else {
+            changeRequest.reduceVoteCount(msg.sender);
+            if (changeRequest.getVoteCount() == 0) {
+                changeRequest.setState(State.changeApproved, msg.sender);
+                emit NewVote(
+                    msg.sender,
+                    acceptChange,
+                    changeRequest.getState(),
+                    "Vote Finished",
+                    changeRequest.getVoteCount()
+                );
+            }
+            else {
+                emit NewVote(
+                    msg.sender,
+                    acceptChange,
+                    changeRequest.getState(),
+                    "Responsible Vote",
+                    changeRequest.getVoteCount()
+                );
+            }
+        }
+
+        changeRequest.setNotAllowedToVote(msg.sender, msg.sender);
+    }
+
+    // Returns the address of the ChangeRequest
+    function getChangeRequestAddress(bytes20 gitHash)
+    public
+    view
+    returns (address changeRequestAddress)
+    {
+        return _changeRequests[gitHash];
     }
 }
--- contracts/ChangeRequest.sol
+++ contracts/ChangeRequest.sol
@@ -1,16 +1,24 @@
 pragma solidity ^0.4.24;
-pragma experimental ABIEncoderV2;
 
 import {ChangeTracker} from './ChangeTracker.sol';
 
 // This contract represents a specific ChangeRequest
 contract ChangeRequest is ChangeTracker {
-    Change private _currentChange;
+    // Privileges
     address private _changeOwner;
     address private _constructionManager;
-    mapping(address => bool) private _allowdToVote;
+
+    // Change data
+    bytes20 private _gitHash;
+    string private _additionalInformation;
+    uint256 private _costs;
+    uint256 private _estimation;
+
+    // Vote state
+    State private _state;
+    mapping(address => bool) private _allowedToVote;
     uint256 private _voteCount;
-    string private _rejectionReason;
+    string private _voteInfo;
 
     // This event gets propagated every time a new Vote happens and tracks the _currentChange.state and the _votesLeft
     event NewVote(
@@ -21,20 +29,23 @@ contract ChangeRequest is ChangeTracker {
         uint256 _votesLeft
     );
 
-    constructor(bytes20 gitHash, string additionalInformation, uint256 costs, uint256 estimation, address manager) public {
-        _currentChange.additionalInformation = additionalInformation;
-        _currentChange.gitHash = gitHash;
-        _currentChange.costs = costs;
-        _currentChange.estimation = estimation;
-        _currentChange.state = State.changeProposed;
+    constructor(
+        bytes20 gitHash,
+        string additionalInformation,
+        uint256 costs,
+        uint256 estimation,
+        address changeOwner,
+        address constructionManager
+    ) public {
+        _changeOwner = changeOwner;
+        _constructionManager = constructionManager;
 
-        _constructionManager = manager;
-        _changeOwner = msg.sender;
-    }
+        _additionalInformation = additionalInformation;
+        _gitHash = gitHash;
+        _costs = costs;
+        _estimation = estimation;
 
-    // Returns the data of the change
-    function viewChange() public view returns (bytes20, string, uint256, uint256, State) {
-        return (_currentChange.gitHash, _currentChange.additionalInformation, _currentChange.costs, _currentChange.estimation, _currentChange.state);
+        _state = State.changeProposed;
     }
 
     // Function can only be run by the owner of the ChangeManager contract (construction manager). The construction
@@ -44,49 +55,104 @@ contract ChangeRequest is ChangeTracker {
     public
     {
         require(msg.sender == _constructionManager);
-        require(_currentChange.state == State.changeProposed);
+        require(_state == State.changeProposed);
         if (acceptChange) {
             _voteCount = responsibleParties.length;
             for (uint i = 0; i < responsibleParties.length; i++) {
-                _allowdToVote[responsibleParties[i]] = true;
+                _allowedToVote[responsibleParties[i]] = true;
             }
-            _currentChange.state = State.changeManaged;
-            emit NewVote(msg.sender, acceptChange, _currentChange.state, "Management Vote", _voteCount);
+            _state = State.changeManaged;
+            emit NewVote(msg.sender, acceptChange, _state, "Management Vote", _voteCount);
         }
         else {
-            _currentChange.state = State.changeRejected;
-            _rejectionReason = rejectionReason;
-            emit NewVote(msg.sender, acceptChange, _currentChange.state, _rejectionReason, 0);
+            _state = State.changeRejected;
+            _voteInfo = rejectionReason;
+            emit NewVote(msg.sender, acceptChange, _state, _voteInfo, 0);
         }
     }
 
     // The allowed parties can vote. As soon as everyone has voted the ChangeRequest is either accepted or rejected
     // TODO: If it's rejected, the changeOwner can amend the ChangeRequest and restart the voting process.
     function responsibleVote(bool acceptChange, string rejectionReason) public {
-        require(_currentChange.state == State.changeManaged);
-        require(_allowdToVote[msg.sender]);
-        _allowdToVote[msg.sender] = false;
+        require(_state == State.changeManaged);
+        require(_allowedToVote[msg.sender]);
+        _allowedToVote[msg.sender] = false;
         if (!acceptChange) {
-            _currentChange.state = State.changeRejected;
-            _rejectionReason = rejectionReason;
-            emit NewVote(msg.sender, acceptChange, _currentChange.state, _rejectionReason, 0);
+            _state = State.changeRejected;
+            _voteInfo = rejectionReason;
+            emit NewVote(msg.sender, acceptChange, _state, _voteInfo, 0);
         }
         else {
             _voteCount = _voteCount - 1;
             if (_voteCount == 0) {
-                _currentChange.state = State.changeApproved;
-                emit NewVote(msg.sender, acceptChange, _currentChange.state, "Vote Finished", _voteCount);
+                _state = State.changeApproved;
+                emit NewVote(msg.sender, acceptChange, _state, "Vote Finished", _voteCount);
             }
             else {
-                emit NewVote(msg.sender, acceptChange, _currentChange.state, "Responsible Vote", _voteCount);
+                emit NewVote(msg.sender, acceptChange, _state, "Responsible Vote", _voteCount);
             }
         }
     }
 
+    function setVoteCount(uint256 voteCount, address setter) public {
+        require(setter == _constructionManager);
+        _voteCount = voteCount;
+    }
+
+    // Problem: Voting is not an atomic function ->
+    //Can reduce vote without removing oneself from the allowedToVote list
+    function reduceVoteCount(address setter) public {
+        require(_allowedToVote[setter]);
+        _voteCount = _voteCount - 1;
+    }
+
+    function getVoteCount() public view returns (uint256){return _voteCount;}
+
+    // Only the manager is allowed to set allowance to vote
+    function setAllowedToVote(address responsibleParty, address setter) public {
+        require(setter == _constructionManager);
+        _allowedToVote[responsibleParty] = true;
+    }
+
+    // Only manager and the responsible party itself are allowed to revoke allowance to vote
+    function setNotAllowedToVote(address responsibleParty, address setter) public {
+        require(setter == _constructionManager || setter == responsibleParty);
+        _allowedToVote[responsibleParty] = false;
+    }
+
+    function isAllowedToVote(address party) public view returns (bool){
+        return _allowedToVote[party];
+    }
+
+    // Problem: responsibleParty can just change the state without caring about voting
+    function setState(State state, address setter) public {
+        require(setter == _constructionManager || _allowedToVote[setter]);
+        _state = state;
+    }
+
+    function getState() public view returns (State){return _state;}
+
+    function setVoteInfo(string voteInfo, address setter) public {
+        require(setter == _constructionManager || _allowedToVote[setter]);
+        _voteInfo = voteInfo;
+    }
+
+    function getVoteInfo() public view returns (string){return _voteInfo;}
+
     // The ChangeRequest has been accepted and can be released.
     function releaseChange() public {
-        require(_currentChange.state == State.changeApproved);
-        _currentChange.state = State.changeReleased;
-        emit NewVote(msg.sender, true, _currentChange.state, "Released", 0);
+        require(_state == State.changeApproved);
+        _state = State.changeReleased;
+        emit NewVote(msg.sender, true, _state, "Released", 0);
+    }
+
+    // Returns the data of the change
+    function viewChange() public view returns (bytes20 gitHash, string additionalInformation, uint256 costs, uint256 estimation) {
+        return (_gitHash, _additionalInformation, _costs, _estimation);
+    }
+
+    // Returns the state of the change
+    function viewState() public view returns (State state, uint256 votesLeft, string rejectionReason) {
+        return (_state, _voteCount, _voteInfo);
     }
 }
